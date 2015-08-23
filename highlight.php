c050f756babae7881ebb062bfd969f77792d2daa
Thomas Breuss <thomasbreuss@gmx.ch>
2015-08-23 17:23:41 +0200

    Utilize new hook system

---
+++ highlight.php
@@ -0,0 +1,48 @@
+<?php
+
+use Herbie\DI;
+use Herbie\Hook;
+
+class HighlightPlugin
+{
+    /**
+     * @return array
+     */
+    public static function install()
+    {
+        $config = DI::get('Config');
+        if ((bool)$config->get('plugins.config.highlight.twig', false)) {
+            Hook::attach('twigInitialized', ['HighlightPlugin', 'addTwigExtension']);
+        }
+        if ((bool)$config->get('plugins.config.highlight.shortcode', true)) {
+            Hook::attach('shortcodeInitialized', ['HighlightPlugin', 'addShortcode']);
+        }
+    }
+
+    public static function addTwigExtension($twig)
+    {
+        $twig->addExtension(new HighlightExtension());
+    }
+
+    public static function addShortcode($shortcode)
+    {
+        $shortcode->add('code', ['HighlightPlugin', 'codeShortcode']);
+    }
+
+    public static function codeShortcode($options, $content)
+    {
+        include_once (__DIR__ . '/vendor/geshi-1.0.8.15/geshi.php');
+
+        $name = empty($options[0]) ? 'text' : $options[0];
+        $geshi = new \GeSHi(trim($content), $name);
+        #$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS);
+        return sprintf(
+            '<div class="highlight highlight-%s">%s</div>',
+            $name,
+            $geshi->parse_code()
+        );
+    }
+
+}
+
+HighlightPlugin::install();
